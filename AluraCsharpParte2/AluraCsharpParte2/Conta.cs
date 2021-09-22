f3af2e4daf4d63573d2e75ba80ab646335f422c5
pedro ali <pedroali78@yahoo.com>
2021-09-22 15:38:31 -0300

    Parte 2 do curso

---
+++ AluraCsharpParte2/AluraCsharpParte2/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraCsharpParte2
+{
+    class Conta
+    {
+        public double saldo = 100;
+        public int agencia;
+        public string nome;
+        
+
+        public void Depositar (double valor)
+        {
+            
+            this.saldo += valor;
+            Console.WriteLine("foi depositado " + valor + " reais na conta de" + this.nome);
+        }
+
+        public bool Sacar (double valor)
+        {
+            
+            if (this.saldo < valor)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+
+            this.saldo -= valor;
+            Console.WriteLine(valor + " reais sacados");
+            return true;
+        }
+
+        public bool Transferir (double valor, Conta contaDestino)
+        {
+            
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ AluraCsharpParte2/AluraCsharpParte2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AluraCsharpParte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta contapedro = new Conta();
+            contapedro.nome = "Pedro Ali";
+            Console.WriteLine("saldo da conta do Pedro é de:" + contapedro.saldo);
+            contapedro.Depositar(200);
+            
+            Console.WriteLine("saldo da conta do Pedro é de:" + contapedro.saldo);
+            contapedro.Sacar(50);
+            Console.WriteLine("saldo da conta do Pedro é de:" + contapedro.saldo);
+
+            Conta contaHenrique = new Conta();
+            contaHenrique.nome = "Hernrique Pinto";
+            contapedro.Transferir(200,contaHenrique);
+        }
+    }
+}
