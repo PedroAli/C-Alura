32853d8f0759b73fd332403328c2a6111ec7b722
pedro ali <pedroali78@yahoo.com>
2021-09-23 14:40:08 -0300

    projeto Criando construtores e membros estaticos

--- AluraCsharpParte2/04-MaosNaMassaCriandoConstrutoresEmembrosEstaticos/ContaCorrente.cs
+++ AluraCsharpParte2/04-MaosNaMassaCriandoConstrutoresEmembrosEstaticos/ContaCorrente.cs
@@ -4,15 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03_MãoNaMassaCriandoGettersESetters
+namespace _04_MaosNaMassaCriandoConstrutoresEmembrosEstaticos
 {
-    class ContaCorrente
+    public class ContaCorrente
     {
-        public Cliente Titular { get; set; } // Lembrar de instanciar o obj Cliente com o new no program
-        public int Agencia { get; set; }
+        
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
         public int Numero { get; set; }
 
         private double _saldo = 100;
+
         public double Saldo
         {
             get
@@ -29,6 +50,16 @@ namespace _03_MãoNaMassaCriandoGettersESetters
                 _saldo = value;
             }
         }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
 
         public bool Sacar(double valor)
         {
---
+++ AluraCsharpParte2/04-MaosNaMassaCriandoConstrutoresEmembrosEstaticos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _04_MaosNaMassaCriandoConstrutoresEmembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(876,12335);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
